4cb89abd71c90f79b313bf29e0f32182dd0b2904
Marcus Liu <me@mail.lmarcus.onl>
2022-01-26 22:48:30 +0800

    docs: 規劃 route 的功能

--- app.js
+++ app.js
@@ -20,10 +20,20 @@ app.get('/', (req, res) => {
 const genHash = require('./genHash')
 
 app.post('/', (req, res) => {
+  // TODO: route post / 
+  // 1. 收 body 的 url，query mongo
+  // 如果有直接跳到 result
+  // 2. query mongo，是否有同樣的的 hashValue，如果沒有則往下做
+  // 有的話，重新產生一次
   const hashValue = genHash()
   console.log(`hashvalue: ${hashValue}`)
   res.render('result', { hashValue })
 })
+
+// TODO: route get /:hashId
+// 1. 收 res.query 的 id，query mongo
+// 如果有 query 到，跳去該頁面
+// 如果沒有 query 到，index render 警告訊息(利用 flag，類似 restaurant list search bar 的方式)
 
 app.listen(port, () => {
   console.log(`URLShortenr is running on http://localhost:${port}`)
